9103e6fd8b7cc586d27d830ed725f92976a3e394
Jeevitha Kannan K S <ksjeevithakannan123@gmail.com>
2025-01-09 10:58:36 +0530

    Add qt theme setup script

---
+++ scripts/qt-theme.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+mkdir -p "$HOME/.config/Kvantum/catppuccin-mocha-blue"
+curl -Lo "$HOME/.config/Kvantum/catppuccin-mocha-blue/catppuccin-mocha-blue.kvconfig" "https://github.com/catppuccin/Kvantum/raw/refs/heads/main/themes/catppuccin-mocha-blue/catppuccin-mocha-blue.kvconfig"
+curl -Lo "$HOME/.config/Kvantum/catppuccin-mocha-blue/catppuccin-mocha-blue.svg" "https://github.com/catppuccin/Kvantum/raw/refs/heads/main/themes/catppuccin-mocha-blue/catppuccin-mocha-blue.svg"
+
+cat <<EOF > "$HOME/.config/Kvantum/kvantum.kvconfig"
+[General]
+theme=catppuccin-mocha-blue
+EOF
+
+mkdir -p "$HOME/.config/qt6ct/colors"
+
+curl -Lo "$HOME/.config/qt6ct/colors/catppuccin-mocha-blue.conf" "https://github.com/catppuccin/qt5ct/raw/refs/heads/main/themes/catppuccin-mocha-blue.conf"
+
+cat <<EOF > "$HOME/.config/qt6ct/qt6ct.conf"
+[Appearance]
+color_scheme_path=$HOME/.config/qt6ct/colors/catppuccin-mocha-blue.conf
+custom_palette=true
+icon_theme=Papirus-Dark
+standard_dialogs=default
+style=kvantum-dark
+
+[Fonts]
+fixed="Noto Sans,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"
+general="Noto Sans,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"
+icon_theme=breeze
+EOF
+
+sudo pacman -S --needed --noconfirm kvantum qt6ct
