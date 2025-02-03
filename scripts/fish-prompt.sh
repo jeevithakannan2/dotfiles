22c60a7ce15f350b2e9c6ee7c5e5bd77be4d5fb3
Jeevitha Kannan K S <ksjeevithakannan123@gmail.com>
2025-02-03 12:06:13 +0530

    add crabfetch package in installation

--- scripts/fish-prompt.sh
+++ scripts/fish-prompt.sh
@@ -1,7 +1,16 @@
 #!/bin/env bash
 
 cd "$DOTFILES" || exit 1
-"$ESCALATION_TOOL" pacman -S --needed --noconfirm fish eza bat ripgrep starship fastfetch zoxide vivid
+
+if ! command -v paru >/dev/null; then
+    git clone https://aur.archlinux.org/paru-bin.git
+    cd paru-bin || exit 1
+    makepkg -si --noconfirm
+fi
+
+paru -S --needed --noconfirm fish eza bat ripgrep starship fastfetch zoxide vivid crabfetch
+
+cd "$DOTFILES" || exit 1
 
 stow_link fish-config
 fish -c "fish_config theme save mocha"
