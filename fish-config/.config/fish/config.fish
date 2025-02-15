939ab5d96393b0fc6b09240e5d3377f1c852a9f0
Jeevitha Kannan K S <ksjeevithakannan123@gmail.com>
2025-02-15 09:55:59 +0530

    feat(fish-prompt): cargo bin path (#65)

--- fish-config/.config/fish/config.fish
+++ fish-config/.config/fish/config.fish
@@ -8,7 +8,8 @@ if status is-interactive
     set -gx XDG_STATE_HOME "$HOME/.local/state"
     set -gx XDG_CACHE_HOME "$HOME/.cache"
 
-    fish_add_path ~/.local/bin
+    fish_add_path "$HOME/.local/bin"
+    fish_add_path "$HOME/.cargo/bin"
 
     if grep -q arch /etc/os-release
         fish_add_path /usr/lib/jvm/default/bin
