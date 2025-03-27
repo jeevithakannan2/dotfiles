9c69494d334de9536f435c30ff4cd1d1ed0054d7
Jeevitha Kannan K S <ksjeevithakannan123@gmail.com>
2025-03-27 13:52:42 +0530

    feat: add alacritty config, multiple terminal emulator prompt on setup (#75)

--- scripts/hypr.sh
+++ scripts/hypr.sh
@@ -7,9 +7,43 @@ install_deps() {
         cd paru-bin
         makepkg -si --noconfirm
     fi
+
     paru
+
+    prompt_te() {
+        info_msg "Choose the terminal emulator you want to use"
+        info_msg "1. Alacritty"
+        info_msg "2. Foot"
+        info_msg "3. Ghostty"
+        printf "Your choice [ 1-3 ]:"
+        read -r choice
+    }
+
+    prompt_te
+
+    while [ "$choice" != "0" ]; do
+        case "$choice" in
+            1)
+                pkg="alacritty"
+                break
+                ;;
+            2)
+                pkg="foot"
+                break
+                ;;
+            3)
+                pkg="ghostty"
+                break
+                ;;
+            *)
+                printf "Enter a valid choice"
+                ;;
+        esac
+        prompt_te
+    done
+
     paru -S --needed --noconfirm hyprland xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper hyprlock hyprshot hypridle \
-        wlogout wttrbar waybar-module-pacman-updates-git foot waybar wofi cliphist mako socat power-profiles-daemon ghostty
+        wlogout wttrbar waybar-module-pacman-updates-git waybar wofi cliphist mako socat power-profiles-daemon "$pkg"
     success_msg "Successfully installed dependencies !!"
 }
 
@@ -20,3 +54,8 @@ fi
 cd "$DOTFILES"
 
 stow_link "hypr-config"
+if [ "$pkg" = "ghostty" ]; then
+    sed -i 's/alacritty/ghostty --gtk-single-instance=true/g' .config/hypr/hyprland/autostart.conf
+else
+    sed -i "/alacritty/$pkg/g" .config/hypr/hyprland/autostart.conf
+fi
