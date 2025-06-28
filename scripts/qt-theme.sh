15a2981a28ba7c976a86d38dc470f73c0d3e9aa6
Jeevitha Kannan K S <ksjeevithakannan123@gmail.com>
2025-06-28 11:46:07 +0530

    fix: wrong variable name (#83)

--- scripts/qt-theme.sh
+++ scripts/qt-theme.sh
@@ -1,21 +1,25 @@
 #!/bin/sh -e
 
-info_msg "Cloning theme files !!"
-mkdir -p "$CONFIG/Kvantum/catppuccin-mocha-blue"
-curl -Lo "$CONFIG/Kvantum/catppuccin-mocha-blue/catppuccin-mocha-blue.kvconfig" "https://github.com/catppuccin/Kvantum/raw/refs/heads/main/themes/catppuccin-mocha-blue/catppuccin-mocha-blue.kvconfig"
-curl -Lo "$CONFIG/Kvantum/catppuccin-mocha-blue/catppuccin-mocha-blue.svg" "https://github.com/catppuccin/Kvantum/raw/refs/heads/main/themes/catppuccin-mocha-blue/catppuccin-mocha-blue.svg"
+info_msg "Installing dependencies !!"
+"$ESCALATION_TOOL" pacman -S --needed --noconfirm kvantum qt6ct
 
-cat <<EOF >"$CONFIG/Kvantum/kvantum.kvconfig"
+info_msg "Cloning theme files !!"
+
+mkdir -p "$CONFIG_DIR/Kvantum/catppuccin-mocha-blue"
+curl -Lo "$CONFIG_DIR/Kvantum/catppuccin-mocha-blue/catppuccin-mocha-blue.kvconfig" "https://github.com/catppuccin/Kvantum/raw/refs/heads/main/themes/catppuccin-mocha-blue/catppuccin-mocha-blue.kvconfig"
+curl -Lo "$CONFIG_DIR/Kvantum/catppuccin-mocha-blue/catppuccin-mocha-blue.svg" "https://github.com/catppuccin/Kvantum/raw/refs/heads/main/themes/catppuccin-mocha-blue/catppuccin-mocha-blue.svg"
+
+cat <<EOF >"$CONFIG_DIR/Kvantum/kvantum.kvconfig"
 [General]
 theme=catppuccin-mocha-blue
 EOF
 
-mkdir -p "$CONFIG/qt6ct/colors"
-curl -Lo "$CONFIG/qt6ct/colors/catppuccin-mocha-blue.conf" "https://github.com/catppuccin/qt5ct/raw/refs/heads/main/themes/catppuccin-mocha-blue.conf"
+mkdir -p "$CONFIG_DIR/qt6ct/colors"
+curl -Lo "$CONFIG_DIR/qt6ct/colors/catppuccin-mocha-blue.conf" "https://github.com/catppuccin/qt5ct/raw/refs/heads/main/themes/catppuccin-mocha-blue.conf"
 
-cat <<EOF >"$CONFIG/qt6ct/qt6ct.conf"
+cat <<EOF >"$CONFIG_DIR/qt6ct/qt6ct.conf"
 [Appearance]
-color_scheme_path=$CONFIG/qt6ct/colors/catppuccin-mocha-blue.conf
+color_scheme_path=$CONFIG_DIR/qt6ct/colors/catppuccin-mocha-blue.conf
 custom_palette=true
 icon_theme=Papirus-Dark
 standard_dialogs=default
@@ -26,8 +30,5 @@ fixed="Noto Sans,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"
 general="Noto Sans,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"
 icon_theme=breeze
 EOF
-
-info_msg "Installing dependencies !!"
-"$ESCALATION_TOOL" pacman -S --needed --noconfirm kvantum qt6ct
 
 success_msg "Theme setup successful !!"
